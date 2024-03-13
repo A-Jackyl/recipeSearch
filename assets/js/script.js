baad5a364f9e3929f6c7b68da8a093d187747530
Michael P <102651473+michmich16@users.noreply.github.com>
2024-03-13 16:36:31 +0100

    function fetch virker ikke

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// MODEL CODE
+// fetch api 
+// function GetAPIData() {
+//     fetch('www.themealdb.com/api/json/v1/1/search.php?f=a')
+
+//         .then((result) => {
+//             console.log(result);
+//             return result.json()
+//         }
+//         )
+
+//         .then((json) => {
+//             console.log(json);
+//             ProductsRecived(json)
+//         });
+// }
+
+
+// CONTROLLER CODE
+
+// VIEW CODE
